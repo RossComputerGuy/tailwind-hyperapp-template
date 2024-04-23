125f852768bdef50328804cbd1147c85370d09bb
Tristan Ross <tristan.ross@midstall.com>
2024-04-22 22:48:58 -0700

    Add Flake package

--- flake.nix
+++ flake.nix
@@ -13,9 +13,24 @@
     flake-utils.lib.eachSystem flake-utils.lib.allSystems (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        inherit (pkgs) lib;
+
+        nodejs = pkgs.nodejs_21;
       in {
+        packages.default = pkgs.buildNpmPackage {
+          pname = "tailwind-hyperapp-template";
+          version = "0-git+${self.shortRev or "dirty"}";
+
+          src = lib.cleanSource self;
+
+          inherit nodejs;
+
+          npmDepsHash = "sha256-uaBCxphJk02f/8nlm6fndFMIWuwsKQ/9Bcwn1MODdY4=";
+        };
+
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ nodejs_21 ];
+          inherit (self.packages.${system}.default) pname version name;
+          packages = [ nodejs ];
         };
       });
 }
